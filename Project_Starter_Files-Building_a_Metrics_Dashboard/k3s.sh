f8f0dff16151ae6d9eda7444efd8183364545bc7
Devi-Babusundaram <devi.babusundaram@coxautoinc.com>
2023-10-24 12:06:34 -0400

    first push

--- Project_Starter_Files-Building_a_Metrics_Dashboard/k3s.sh
+++ Project_Starter_Files-Building_a_Metrics_Dashboard/k3s.sh
@@ -5,4 +5,4 @@ echo "**** Begin installing k3s"
 curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION=v1.19.5+k3s1 K3S_KUBECONFIG_MODE="644" sh -
 echo "**** End installing k3s"
 
-#kubectl proxy --address='0.0.0.0' /dev/null &
+kubectl proxy --address='0.0.0.0' /dev/null &
